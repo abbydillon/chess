0f3d745ccadf266c38ddc7a305fe7fff015c3172
abby <dillon.abby@gmail,com>
2026-09-18 20:19:27 -0600

    add start and end position variables

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -8,8 +8,15 @@ package chess;
  */
 public class ChessMove {
 
+    private ChessPosition startPosition;
+    private ChessPosition endPosition;
+
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+
+        this.startPosition = startPosition;
+        this.endPosition = endPosition;
+
     }
 
     /**
